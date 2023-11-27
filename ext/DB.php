785549b3182aa4852af6f09ef2751440cd8572e3
raul faria <raul@faria.ch>
2023-11-27 08:00:16 +0100

    finished backend

--- ext/DB.php
+++ ext/DB.php
@@ -11,19 +11,200 @@ class DB
     private string $DB_PASSWORD = 'FEA9PNz3p+tu+8!?MPrP';
     private string $DB_NAME = 'winnert1_m295raul';
     private mysqli $conn;
+    private string $table;
+    private string $column;
+    private string $id;
 
-    public function __construct() {
-        $this->conn = new mysqli($this->DB_HOST, $this->DB_USER, $this->DB_PASSWORD, $this->DB_NAME);
+    public function __construct($table, $column, $id) {
+        header('Content-Type: application/json');
 
-        $this->conn->set_charset('utf8');
+        try {
+            $this->conn = new mysqli($this->DB_HOST, $this->DB_USER, $this->DB_PASSWORD, $this->DB_NAME);
 
-        if ($this->conn->connect_error) {
-            die("Connection failed: ". $this->conn->connect_error);
+            if ($this->conn->connect_error) {
+                throw new \Exception("Connection failed: " . $this->conn->connect_error);
+            }
+
+            if (!$this->conn->set_charset('utf8')) {
+                throw new \Exception("Error setting charset: " . $this->conn->error);
+            }
+
+            $this->table = $table ?? '';
+            $this->column = $column ?? '';
+            $this->id = $id ?? '';
+
+            $this->start();
+        } catch (\Exception $e) {
+            $this->handleError($e->getMessage());
         }
     }
 
-    public function getConn(): mysqli
-    {
+    private function start(): void {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'POST':
+                    $this->insertData();
+                    break;
+
+                case 'PUT':
+                case 'PATCH':
+                    $this->updateData();
+                    break;
+
+                case 'DELETE':
+                    $this->deleteData();
+                    break;
+
+                case 'GET':
+                    $this->getData();
+                    break;
+
+                default:
+                    http_response_code(400);
+            }
+        } catch (\Exception $e) {
+            $this->handleError($e->getMessage());
+        }
+    }
+
+private function getData() {
+        try {
+            if ($this->id && $this->column) {
+                $query = 'SELECT * FROM tbl_' . $this->table . ' WHERE ' . $this->column . ' = ' . $this->id . ';';
+            } else {
+                $query = 'SELECT * FROM tbl_' . $this->table . ';';
+            }
+    
+            $result = $this->conn->query($query);
+    
+            if ($result === false) {
+                throw new \Exception('Error in query: ' . $this->conn->error);
+            }
+    
+            $data = [];
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+    
+            http_response_code(200);
+            echo json_encode(['data' => $data]);
+        } catch (\Exception $e) {
+            $this->handleError($e->getMessage());
+        }
+    }
+
+
+    private function deleteData() {
+        try {
+            if (!empty($this->id) && !empty($this->column)) {
+                $query = 'DELETE FROM tbl_' . $this->table . ' WHERE ' . $this->column . ' = ' . $this->id . ';';
+                $result = $this->conn->query($query);
+
+                if ($result === false) {
+                    throw new \Exception('Error deleting record: ' . $this->conn->error);
+                }
+
+                echo json_encode(['message' => 'Record deleted successfully']);
+            } else {
+                throw new \Exception('Invalid parameters for delete operation');
+            }
+        } catch (\Exception $e) {
+            $this->handleError($e->getMessage());
+        }
+    }
+    
+private function updateData() {
+        try {
+            // Get the request body
+            $requestData = json_decode(file_get_contents('php://input'), true);
+    
+            // Check if request body is valid JSON
+            if ($requestData === null && json_last_error() !== JSON_ERROR_NONE) {
+                throw new \Exception('Invalid JSON data in request body');
+            }
+    
+            // Ensure the required data is present
+            if (empty($requestData)) {
+                throw new \Exception('No data provided for update');
+            }
+    
+            // Construct the SET part of the SQL query
+            $setValues = [];
+            foreach ($requestData as $key => $value) {
+                $key = $this->conn->real_escape_string($key);
+                $value = $this->conn->real_escape_string($value);
+                $setValues[] = "$key = '$value'";
+            }
+    
+            $setValuesString = implode(', ', $setValues);
+    
+            // Construct the SQL query
+            $whereClause = (!empty($this->id) && !empty($this->column)) ? "WHERE $this->column = $this->id" : '';
+            $query = "UPDATE tbl_$this->table SET $setValuesString $whereClause;";
+    
+            // Execute the query
+            $result = $this->conn->query($query);
+    
+            // Check if the query was successful
+            if ($result === false) {
+                throw new \Exception('Error updating record: ' . $this->conn->error);
+            }
+    
+            // Return a success message
+            echo json_encode(['message' => 'Record updated successfully']);
+        } catch (\Exception $e) {
+            $this->handleError($e->getMessage());
+        }
+    }
+
+
+    private function insertData() {
+        try {
+            // Get the request body
+            $requestData = json_decode(file_get_contents('php://input'), true);
+    
+            // Check if request body is valid JSON
+            if ($requestData === null && json_last_error() !== JSON_ERROR_NONE) {
+                throw new \Exception('Invalid JSON data in request body');
+            }
+    
+            // Ensure the required data is present
+            if (empty($requestData)) {
+                throw new \Exception('No data provided for insert');
+            }
+    
+            // Escape column names and values
+            $columns = array_map([$this->conn, 'real_escape_string'], array_keys($requestData));
+            $values = array_map([$this->conn, 'real_escape_string'], array_values($requestData));
+    
+            // Construct the SQL query
+            $columnsString = implode(', ', $columns);
+            $valuesString = implode("', '", $values);
+    
+            $query = "INSERT INTO tbl_$this->table ($columnsString) VALUES ('$valuesString');";
+    
+            // Execute the query
+            $result = $this->conn->query($query);
+    
+            // Check if the query was successful
+            if ($result === false) {
+                throw new \Exception('Error inserting record: ' . $this->conn->error);
+            }
+    
+            // Return a success message
+            echo json_encode(['message' => 'Record inserted successfully']);
+        } catch (\Exception $e) {
+            $this->handleError($e->getMessage());
+        }
+    }
+
+
+    private function handleError($message) {
+        http_response_code(500);
+        echo json_encode(['error' => $message]);
+    }
+
+    public function getConn(): mysqli {
         return $this->conn;
     }
 }
--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
+use ext\DB;
 use ext\Sanitize;
 
-// enable error reporting
+// Enable error reporting
+ini_set('display_errors', 'On');
 error_reporting(E_ALL & ~E_NOTICE);
 
-// config
+// Config
 define('ABSPATH', dirname(__FILE__));
+$tables = ['lehrbetriebe', 'lernende', 'lehrbetrieb_lernende', 'laender', 'dozenten', 'kurse', 'kurse_lernende'];
 
-// load classes
+// Load classes
 require('ext/Sanitize.php');
+require('ext/DB.php');
 
-// get request uri
+// Get request URI
 $requestUrl = filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL);
 $requestUrl = parse_url($requestUrl);
 
@@ -21,66 +25,32 @@ $query = ($requestUrl['query'] ?? '');
 define('REQUESTURI', $path);
 define('REQUESTQUERY', $query);
 
-
-// routing
+// Routing
 $requestView = '';
 
-// routing: home
-if(REQUESTURI === '' OR REQUESTURI === 'home')
-{
-    	$requestView = ABSPATH.'/app/home/index.php';
+// Routing: home
+if (REQUESTURI === '' OR REQUESTURI === 'home') {
+    $requestView = ABSPATH . '/app/home/index.php';
 }
 
-// routing: other views
-else
-{
-    	// split path : get parameters and count
-	$split_requesturi = explode('/', REQUESTURI);
+// Routing: other views
+else {
+    // Split path : get parameters and count
+    $split_requesturi = explode('/', REQUESTURI);
 
-	// sanitize params
-	$route_folder = Sanitize::sanitizeRouteFolder($split_requesturi[0]);
-	$route_id = Sanitize::sanitizeRouteId($split_requesturi[1]);
+    // Sanitize params
+    $table = isset($split_requesturi[0]) ? Sanitize::sanitizeRouteFolder($split_requesturi[0]) : null;
+    $column = isset($split_requesturi[1]) ? Sanitize::sanitizeRouteFolder($split_requesturi[1]) : null;
+    $id = isset($split_requesturi[2]) ? Sanitize::sanitizeRouteFolder($split_requesturi[2]) : null;
 
-	// 1 parameter in the query string : index, create
-	if(count($split_requesturi) === 1)
-	{
-    		if($_SERVER['REQUEST_METHOD'] === 'GET')
-		{
-        define('REQUESTID', 'all');
-        			$requestView = ABSPATH.'/app/'.$route_folder.'/read.php';
-		}
-		else if($_SERVER['REQUEST_METHOD'] === 'POST')
-        		{
-        			$requestView = ABSPATH.'/app/'.$route_folder.'/create.php';
-		}
-	}
-	
-	// 2 parameters in the query string : read, update, delete
-	else if(count($split_requesturi) === 2)
-    	{
-    		define('REQUESTID', $route_id);
-    		
-		if($_SERVER['REQUEST_METHOD'] === 'GET')
-		{
-        			$requestView = ABSPATH.'/app/'.$route_folder.'/read.php';
-		}
-		else if($_SERVER['REQUEST_METHOD'] === 'PUT')
-        		{
-        			$requestView = ABSPATH.'/app/'.$route_folder.'/update.php';
-		}
-		else if($_SERVER['REQUEST_METHOD'] === 'DELETE')
-        		{
-        			$requestView = ABSPATH.'/app/'.$route_folder.'/delete.php';
-		}
-	}
+    define('TABLE', $table);
+    define('COLUMN', $column);
+    define('ID', $id);
 }
 
-// routing: view or error
-if(file_exists($requestView))
-{
-    	require_once($requestView);
-}
-else
-{
-    	require_once(ABSPATH.'/app/error/not_found.php');
+// Routing: view or error
+if (in_array(TABLE, $tables)) {
+    $db = new DB(TABLE, COLUMN, ID);
+} else {
+    require_once(ABSPATH . '/app/error/not_found.php');
 }
